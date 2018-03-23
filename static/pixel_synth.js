fcae2e8f8b57d47388be64cfc05c260691d954bf
macintoshpie <ted.summer2@gmail.com>
2018-03-22 21:32:21 -0500

    mobile

--- static/pixel_synth.js
+++ static/pixel_synth.js
@@ -36,6 +36,12 @@ can lock an attribute down, so that clicking and dragging on screen only affects
 
 // Draw your own 2d filter??? that may be nice.
 
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
 //sum notes
 var aScale = [
 277.2,	554.4,	1109,	2217,
@@ -57,9 +63,12 @@ function crappy(ar, r, c) {
 aScale = crappy(aScale, 5, 4)
 
 //create a synth and connect it to the master output (your speakers)
-var red_syn = new Tone.Oscillator(440, "square12").toMaster().start();
-var grn_syn = new Tone.Oscillator(440, "sine12").toMaster().start();
-var blu_syn = new Tone.Oscillator(440, "triangle12").toMaster().start();
+var red_syn; 
+var grn_syn;
+var blu_syn;
+
+var isMobile;
+
 ///////
 // VARS
 var myCanvas;
@@ -380,6 +389,15 @@ function initialize() {
     	});
     });
 
+
+    //SETUP SYNTHS
+    isMobile = window.mobilecheck()
+    if (isMobile) {
+	    red_syn = new Tone.Oscillator(440, "square12").toMaster().start();
+		grn_syn = new Tone.Oscillator(440, "sine12").toMaster().start();
+		blu_syn = new Tone.Oscillator(440, "triangle12").toMaster().start();
+	}
+
 }
 
 function updateKnobs() {
@@ -468,15 +486,12 @@ function updateImage() {
 		
 
 		basic_unit = [
-			// red_list[i%red_list.length] * red_mod[i%red_mod.length] * red_weight * 255,
-			// green_list[i%green_list.length] * green_mod[(i+ 2)%green_mod.length] * green_weight * 255,
-			// blue_list[i%blue_list.length] * blue_mod[(i+ 1)%blue_mod.length] * blue_weight * 255,
-			// 255
 			red_chn.getVal(i, true, 0) * MAX_RGB_VAL,
 			grn_chn.getVal(i, true, 0) * MAX_RGB_VAL,
 			blu_chn.getVal(i, true, 0) * MAX_RGB_VAL,
 			MAX_RGB_VAL
 		]
+		
 		var disp_arr = []
 		for (var display_i=0; display_i<display_scale; display_i++){
 			disp_arr = disp_arr.concat(basic_unit)
@@ -504,20 +519,9 @@ function updateImage() {
 		// imgData.data[img_idx+2] = blue_list[i%blue_list.length] * blue_weight
 	}
 
-	yuh = red_chn.getVal(i, true, 0)
-	ggg = yuh ? Math.floor(yuh * aScale.length) : 0
-	red_syn.frequency.value = aScale[ggg];
-	red_syn.volume.value = red_chn.weight * 100 - 100
-
-	asd = grn_chn.getVal(i, true, 0)
-	fff = asd ? Math.floor(asd * aScale.length) : 0
-	grn_syn.frequency.value = aScale[fff];
-	grn_syn.volume.value = grn_chn.weight * 100 - 100
-
-	jkl = blu_chn.getVal(i, true, 0)
-	hhh = jkl ? Math.floor(jkl * aScale.length) : 0
-	blu_syn.frequency.value = aScale[hhh];
-	blu_syn.volume.value = blu_chn.weight * 100 - 100
+	if (isMobile){
+		updateSynths(i)
+	}
 
 	//TODO: these shifts should be determined by the list length
 	// specifically, what would be the next value after writing the last (bottom right) pixel?
@@ -548,6 +552,23 @@ function updateImage() {
 	red_chn.stepAll(i)
 	grn_chn.stepAll(i)
 	blu_chn.stepAll(i)
+}
+
+function updateSynths(i) {
+	yuh = red_chn.getVal(i, true, 0)
+	ggg = yuh ? Math.floor(yuh * aScale.length) : 0
+	red_syn.frequency.value = aScale[ggg];
+	red_syn.volume.value = red_chn.weight * 100 - 100
+
+	asd = grn_chn.getVal(i, true, 0)
+	fff = asd ? Math.floor(asd * aScale.length) : 0
+	grn_syn.frequency.value = aScale[fff];
+	grn_syn.volume.value = grn_chn.weight * 100 - 100
+
+	jkl = blu_chn.getVal(i, true, 0)
+	hhh = jkl ? Math.floor(jkl * aScale.length) : 0
+	blu_syn.frequency.value = aScale[hhh];
+	blu_syn.volume.value = blu_chn.weight * 100 - 100
 }
 
 function draw() {
