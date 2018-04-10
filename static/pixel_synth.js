308dc63b804d4b7baf9540e10cc528e47ed67d48
macintoshpie <ted.summer2@gmail.com>
2018-04-10 16:06:23 -0500

    another update

--- static/pixel_synth.js
+++ static/pixel_synth.js
@@ -36,6 +36,13 @@ can lock an attribute down, so that clicking and dragging on screen only affects
 
 // Draw your own 2d filter??? that may be nice.
 
+/*
+TODO:
+figure out a good layout for computer and mobile
+make all controls accessible (eg feedback)
+
+*/
+
 window.mobilecheck = function() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -54,7 +61,6 @@ function crappy(ar, r, c) {
 	yy = []
 	for (i=0; i<c; i++) {
 		for (j=0; j<r; j++) {
-			console.log(i+(c*j))
 			yy.push(ar[i+(c*j)])
 		}
 	}
@@ -62,7 +68,6 @@ function crappy(ar, r, c) {
 }
 aScale = crappy(aScale, 5, 4)
 
-//create a synth and connect it to the master output (your speakers)
 var red_syn; 
 var grn_syn;
 var blu_syn;
@@ -81,9 +86,7 @@ var data_height = 40;
 var ctx;
 var imgData;
 var displayImage;
-var display_scale = 10;
-var display_unit = new Array(display_scale*4);
-display_unit = [255, 0, 0, 255, 255, 0, 0, 255];
+var display_scale;
 red_pixel = [255, 0, 0, 255]
 var red_freq = 1
 var green_freq = 1
@@ -106,18 +109,45 @@ var red_weight = 1
 var green_weight = 0
 var blue_weight = 0
 
-window.onload = initialize;
-
 var mouseIsDown = false;
-var editingMod = false; // indicates when user is updating mod value rather than a main channel
 
 var MAX_RGB_VAL = 255;
 var KNOB_THICKNESS = "1"
+var KNOB_WIDTH = 140
+var KNOB_HEIGHT = KNOB_WIDTH
 
-var red_chn = new Channel("red", new Wave(triangle(294), 293), 1)
-var grn_chn = new Channel("green", new Wave(triangle(200), 10), 1)
-var blu_chn = new Channel("blue", new Wave(saw(30), 1), 0)
+var mod_1 = new Wave("mod 1", saw(10), 1)
+var mod_2 = new Wave("mod 2", saw(5), 1)
+var red_chn = new Channel("red", new Wave("red", triangle(294), 293), 1)
+var grn_chn = new Channel("green", new Wave("green", triangle(200), 10), 1)
+var blu_chn = new Channel("blue", new Wave("blue", saw(30), 1), 0)
+
+
 var current_chn = red_chn;
+var modSrcSelect;
+
+var wave_name_map = {
+	'red': {
+		'src': red_chn,
+		'color': "#FF0000"
+	},
+	'green': {
+		'src': grn_chn,
+		'color': "#00FF00"
+	},
+	'blue': {
+		'src': blu_chn,
+		'color': "#0000FF"
+	},
+	'mod 1': {
+		'src': mod_1,
+		'color': "#8b8b8b"
+	},
+	'mod 2': {
+		'src': mod_2,
+		'color': "#8b8b8b"
+	}
+}
 
 /// Cool stuff
 // weave pattern:
@@ -128,7 +158,12 @@ var twoD = [[.1, .5, .1], [.5, 1, .5], [.1, .5, .1]]
 var manTimes = 100
 var man = 0
 
-function Wave(wave_list, frequency) {
+window.onload = initialize;
+
+
+
+function Wave(label, wave_list, frequency) {
+	this.label = label;
 	this.wave_list = wave_list;
 	this.frequency = frequency;
 	this.wave_function = triangle
@@ -140,8 +175,12 @@ function Wave(wave_list, frequency) {
 		//f = this.frequency + Math.floor(negPosInt(mod) * 3) //will need to replace constant with a variable "magnitude"
 		//t = this.wave_list.splice(0, f)
 		//this.wave_list = this.wave_list.concat(t);
+
 		m = Math.floor(negPosInt(mod) * mod_amt)
-		//ADD m BELOW TO MODIPHY FREQUENCY WITH THE MOD PARAMETER
+		if (mod < 0) {
+			//alert("ALERTTTT")
+		}
+		//ADD m BELOW TO MODIFY FREQUENCY WITH THE MOD PARAMETER
 		this.current_idx = (this.current_idx + this.frequency + m)%this.wave_list.length // THIS IS WHAT MODIFIES PHASE!!
 		if (isNaN(this.current_idx)) throw "Parameter is not a number!";
 	}
@@ -157,32 +196,44 @@ function Wave(wave_list, frequency) {
 }
 
 function Channel(color_str, wave, weight) {
-	this.color = color_str;
+	this.label = color_str;
 	this.wave = wave;
 	this.weight = weight;
-	this.mod = new Wave(saw(10), 1);
+	this.mod = mod_1;//new Wave(saw(10), 1);
 	this.phase_mod_amt = 0; // Determines the magnitude of the modulation on the wave
 	this.freq_mod_amt = 0;
 	// Mod Destinations:
 	// - Frequency of wave
 	// - phase of wave
-	
+	this.mod_dest = "phase"
 
-	this.getVal = function(index, with_weight, recursion_depth) {
-		i = index;
-		//DO SOMETHING WITH RECURSION DEPTH
-		if (recursion_depth > 1) {
-			//console.log("init r: ", recursion_depth)
-		}
-		
+	this.getMod = function(index, recursion_depth) {
 		if (recursion_depth > MAX_RECURSION) {
 			m = 0; // NOT applying any mod since we've recursed to the max
 		}
 		else {
 			rd = recursion_depth + 1
-			m = Math.floor(negPosInt(this.mod.getVal(i, false, rd)) * this.phase_mod_amt); // this is used to mod the phase
+			m = this.mod.getVal(i, false, rd);
 		}
-		w = this.wave.getVal(i + m, false)
+		return m;
+	}
+	
+
+	this.getVal = function(index, with_weight, recursion_depth) {
+		i = index;
+
+		if (this.mod_dest == "phase") {
+			m = Math.floor(negPosInt(this.getMod(i, recursion_depth)) * this.phase_mod_amt)
+			w = this.wave.getVal(i + m, false)
+		}
+		else if (this.mod_dest == "weight") {
+			m = this.getMod(index, recursion_depth) * this.phase_mod_amt
+			w = this.wave.getVal(i, false) * m
+		}
+		else {
+			w = this.wave.getVal(i, false)
+		}
+		//w = this.wave.getVal(i + m, false)
 		if (with_weight) { //this will apply weighting and other filters
 			//if (m!=0) console.log(m)
 			//m = 0//(Math.abs(m) == 0) ? 0 : m;
@@ -196,7 +247,14 @@ function Channel(color_str, wave, weight) {
 	}
 
 	this.stepAll = function(index) {
-		this.wave.step(this.mod.getVal(index, false, 0), this.freq_mod_amt);
+		if (this.mod_dest == "frequency") {
+			m = this.getMod(index, 0)
+		}
+		else {
+			m = 0.5 // 0.5 means no mod b/c it is halfway between 0 and 1
+		}
+		
+		this.wave.step(m, this.phase_mod_amt)//this.mod.getVal(index, false, 0), this.phase_mod_amt);
 		if (this.mod instanceof Wave) {
 			this.mod.step(0.5, 0); // currently not modding mods... (.5 means no mod b/c halfway between 0 and 1)
 		}
@@ -206,9 +264,15 @@ function Channel(color_str, wave, weight) {
 function initialize() {
 	// myCanvas = document.createElement("canvas");
 	// document.body.appendChild(myCanvas);
+
+
 	myCanvas = document.getElementById("myCanvas")
+	disp_width = $("#myCanvas").parent().width()
+	disp_height = disp_width
+
 	myCanvas.width = disp_width;
 	myCanvas.height = disp_height;
+	display_scale = Math.floor(disp_width / data_width)
 	ctx = myCanvas.getContext("2d");
 	
 	imgData = ctx.createImageData(data_width, data_height);
@@ -240,24 +304,37 @@ function initialize() {
 	//Add inputs
 	waveSelect = document.getElementById("waveSelect")
 	waveSelect.addEventListener('change', function() {
-		if (!editingMod) {
-			w = current_chn.wave
+		if (current_chn instanceof Wave) {
+			w = current_chn
 		} else {
-			if (current_chn.mod instanceof Wave) {
-				w = current_chn.mod
-			} else {
-				w = current_chn.mod.wave
-			}
-			
+				w = current_chn.wave
 		}
 		w.wave_function = eval(waveSelect.value)
 		l = w.wave_list.length
-		w.wave_list = current_chn.wave.wave_function(l)
+		w.wave_list = w.wave_function(l)
 	})
+
+	// Mod source
+	modSrcSelect = $("#modSourceSelect")
+	modSrcSelect.change(function() {
+		if (current_chn instanceof Channel) {
+			nmod = this.value
+			current_chn.mod = wave_name_map[this.value]["src"]
+		}
+	})
+
+	// Mod destination
+	modDestSelect = $("#modDestinationSelect")
+	modDestSelect.change(function() {
+		if (current_chn instanceof Channel) {
+			ndest = this.value
+			current_chn.mod_dest = this.value
+		}
+	})
+
 
 	rb = document.getElementById("rb")
 	rb.addEventListener('click', function(e) {
-		editingMod = false;
 		current_chn = red_chn
 		waveSelect.value = red_chn.wave.wave_function.name
 		updateKnobs();
@@ -265,7 +342,6 @@ function initialize() {
 
 	gb = document.getElementById("gb")
 	gb.addEventListener('click', function(e) {
-		editingMod = false;
 		current_chn = grn_chn
 		waveSelect.value = grn_chn.wave.wave_function.name
 		updateKnobs();
@@ -273,22 +349,24 @@ function initialize() {
 
 	bb = document.getElementById("bb")
 	bb.addEventListener('click', function(e) {
-		editingMod = false;
 		current_chn = blu_chn
 		waveSelect.value = blu_chn.wave.wave_function.name
 		updateKnobs();
 	})
 
-	mbtn = document.getElementById("mod")
-	mbtn.addEventListener('click', function(e) {
-		editingMod = true;
-		if (current_chn.mod instanceof Wave) {
-			waveSelect.value = current_chn.mod.wave_function.name
-		} else {
-			waveSelect.value = current_chn.mod.wave.wave_function.name
-		}
+	m1b = $("#m1btn")
+	m1b.click(function() {
+		console.log("asdf")
+		current_chn = mod_1;
+		waveSelect.value = mod_1.wave_function.name
 		updateKnobs();
-		
+	})
+
+	m2b = $("#m2btn")
+	m2b.click(function() {
+		current_chn = mod_2;
+		waveSelect.value = mod_2.wave_function.name
+		updateKnobs();
 	})
 
 	document.addEventListener('keydown', function(event) {
@@ -345,77 +423,134 @@ function initialize() {
 
 	// WAVE LENGTH
 	$(function() {
-        $("#dial-wave").knob({
-        	'step': '.2',
-        	'thickness': KNOB_THICKNESS,
-        	'change': function(v) {updateCurrentWave(Math.floor(v/100*max_list_len))}
-    	});
-    });
+		$("#dial-wave").knob({
+			'step': '.2',
+			'width': KNOB_WIDTH,
+			'height': KNOB_HEIGHT,
+			"inputColor": "000000",
+			'thickness': KNOB_THICKNESS,
+			'change': function(v) {updateCurrentWave(Math.floor(v/100*max_list_len))}
+		});
+	});
 
 	// FREQUENCY
-    $(function() {
-        $("#dial-frequency").knob({
-        	'thickness': KNOB_THICKNESS,
-        	'change': function(v) {
-        		if (editingMod) {
-        			if (current_chn.mod instanceof Wave) {
-	        			wl = current_chn.mod.wave_list.length
-	        		} else {
-	        			wl = current_chn.mod.wave.wave_list.length
-	        		}
-        		} else {
-        			wl = current_chn.wave.wave_list.length
-        		}
-        		
-        		freqVal = Math.floor(v/100*wl)
-        		updateCurrentWave(null, freqVal)
-        	}
-    	});
-    });
+	$(function() {
+		$("#dial-frequency").knob({
+			'width': KNOB_WIDTH,
+			'height': KNOB_HEIGHT,
+			"inputColor": "000000",
+			'thickness': KNOB_THICKNESS,
+			'change': function(v) {
+				if (current_chn instanceof Wave) {
+					wl = current_chn.wave_list.length
+				} else {
+					wl = current_chn.wave.wave_list.length
+				}
+				
+				freqVal = Math.floor(v/100*wl)
+				updateCurrentWave(null, freqVal)
+			}
+		});
+	});
 
-    // MOD AMT
-    $(function() {
-        $("#dial-mod-phase").knob({
-        	'thickness': KNOB_THICKNESS,
-        	'change': function(v) {current_chn.phase_mod_amt=v}
-    	});
-    });
+	// MOD AMT
+	$(function() {
+		$("#dial-mod-phase").knob({
+			'width': KNOB_WIDTH,
+			'height': KNOB_HEIGHT,
+			"inputColor": "000000",
+			'thickness': KNOB_THICKNESS,
+			'change': function(v) {current_chn.phase_mod_amt=v}
+		});
+	});
 
-    // CHANNEL WEIGHT
-    $(function() {
-        $("#dial-weight").knob({
-        	'thickness': KNOB_THICKNESS,
-        	'change': function(v) {current_chn.weight=v/100}
-    	});
-    });
+	// CHANNEL WEIGHT
+	$(function() {
+		$("#dial-weight").knob({
+			'width': KNOB_WIDTH,
+			'height': KNOB_HEIGHT,
+			"inputColor": "000000",
+			'thickness': KNOB_THICKNESS,
+			'change': function(v) {current_chn.weight=v/100}
+		});
+	});
 
 
-    //SETUP SYNTHS
-    isMobile = window.mobilecheck()
-    if (isMobile) {
-	    red_syn = new Tone.Oscillator(440, "square12").toMaster().start();
+	//SETUP SYNTHS
+	isMobile = window.mobilecheck()
+	if (!isMobile) {
+		red_syn = new Tone.Oscillator(440, "square12").toMaster().start();
 		grn_syn = new Tone.Oscillator(440, "sine12").toMaster().start();
 		blu_syn = new Tone.Oscillator(440, "triangle12").toMaster().start();
 	}
 
+	// Set everything properly at first
+	updateKnobs()
+
 }
 
 function updateKnobs() {
-	if (editingMod) {
-		if (current_chn.mod instanceof Wave) {
-			wav = current_chn.mod
-		} else {
-			wav = current_chn.mod.wave
-		}
-		
+	if (current_chn instanceof Wave) {
+		wav = current_chn
 	} else  {
 		wav = current_chn.wave
 	}
+	console.log(wave_name_map[current_chn.label]['color'])
+	
+	$('.dial').trigger(
+		'configure',
+		{
+			"fgColor": wave_name_map[current_chn.label]['color']
+		}
+	);
 	$("#dial-wave").val(wav.wave_list.length * 100 / max_list_len).trigger('change')
 	$("#dial-frequency").val(wav.frequency * 100 / wav.wave_list.length).trigger('change')
+	
 
-	$("#dial-mod-phase").val(current_chn.phase_mod_amt).trigger('change')
-	$("#dial-weight").val(current_chn.weight * 100).trigger('change')
+	// get mod source and destination
+	if (current_chn instanceof Channel) {
+		// Update mod knobs
+		$("#dial-mp-container").css('visibility', 'visible');
+		$("#dial-weight-container").css('visibility', 'visible');
+		$("#dial-mod-phase").val(current_chn.phase_mod_amt).trigger('change')
+		$("#dial-weight").val(current_chn.weight * 100).trigger('change')
+
+		// Update the mod selector
+		modSrcSelect.val(current_chn.mod.label)
+		modSrcSelect.prop( "disabled", false );
+		//$("#modSourceSelect option:contains('" + current_chn.label + "')").attr("disabled","disabled");//.attr('disabled', 'disabled')//removeAttr('disabled');
+		$("#modSourceSelect option").each(function() {
+			if ($(this)[0].innerText == current_chn.label) {
+				$(this).attr("disabled", "disabled")
+			} else {
+				$(this).removeAttr("disabled")
+			}
+		})
+		modDestSelect.val(current_chn.mod_dest)
+		modDestSelect.prop( "disabled", false );
+	} else {
+		// Update mod knobs
+		$("#dial-mp-container").css('visibility', 'hidden');
+		$("#dial-weight-container").css('visibility', 'hidden');
+
+		// Update mod selector
+		modSrcSelect.val("None")
+		modSrcSelect.prop( "disabled", true );
+		modDestSelect.val("None")
+		modDestSelect.prop( "disabled", true );
+	}
+
+	//Update button colors
+	// $(".btn").each(function() {
+	// 	n = $(this)[0].innerText.toLowerCase()
+	// 	if (current_chn.label == n) {
+	// 		$(this).css('background-color', wave_name_map[n]['color']);
+	// 	}
+	// 	else {
+	// 		console.log("nope")
+	// 	}
+	// })
+	
 }
 
 function myMod(n, m) {
@@ -438,14 +573,10 @@ function negPosInt(x) {
 
 
 function updateCurrentWave(waveLen, freqVal) {
-	if (!editingMod) {
-		w = current_chn.wave
+	if (current_chn instanceof Wave) {
+		w = current_chn
 	} else {
-		if (current_chn.mod instanceof Wave) {
-			w = current_chn.mod
-		} else {
-			w = current_chn.mod.wave
-		}
+		w = current_chn.wave
 	}
 	if (waveLen && waveLen != w.wave_list.length) {
 		w.wave_list = w.wave_function(waveLen)
@@ -491,7 +622,7 @@ function updateImage() {
 			blu_chn.getVal(i, true, 0) * MAX_RGB_VAL,
 			MAX_RGB_VAL
 		]
-		
+
 		var disp_arr = []
 		for (var display_i=0; display_i<display_scale; display_i++){
 			disp_arr = disp_arr.concat(basic_unit)
@@ -519,7 +650,7 @@ function updateImage() {
 		// imgData.data[img_idx+2] = blue_list[i%blue_list.length] * blue_weight
 	}
 
-	if (isMobile){
+	if (!isMobile){
 		updateSynths(i)
 	}
 
