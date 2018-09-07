2c6bc7f4598bf67921da72d78c6cdef7c65605ed
macintoshpie <ted.summer2@gmail.com>
2018-09-06 21:36:38 -0500

    refactor(root): serve new index

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, send_file
 app = Flask(__name__)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('indexNew.html')
 
 if __name__ == '__main__':
     app.run()
